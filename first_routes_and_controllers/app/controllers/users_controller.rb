5b127c38089ea59017446c0cafb10138f20d10aa
rtexelm <texelist@gmail.com>
2022-10-19 21:20:02 -0400

    Completed TTBOMK

--- first_routes_and_controllers/app/controllers/users_controller.rb
+++ first_routes_and_controllers/app/controllers/users_controller.rb
@@ -3,15 +3,42 @@
 class UsersController < ApplicationController
 
   def index
-    render plain: "I'm the index action"
+    @users = User.all
+
+    render json:@users
   end
 
   def create
-    render json: params
+
+    user = User.new(params.require(:user).permit(:name, :email))
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def show
-    render json: params
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(params.require(:user).permit(:name, :email))
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id].permit)
+    if user.destroy #(params.require(:user).permit(:name, :email))
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
 end
